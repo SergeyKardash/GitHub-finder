5bcb2b2c4784c7d5a078c4f2223e0d022fe4327f
Sergey Kardash <kardashsergeydev@gmail.com>
2018-01-17 18:35:13 +0200

    show repositories

--- github.js
+++ github.js
@@ -2,14 +2,20 @@ class GitHub {
   constructor () {
     this.client_id = '94e4a1d4fcd694e71e7a';
     this.client_secret = '24791bb8ed4e37997ef2e33e7c0852f67ce2ce03';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc'
   }
 
   async getUser(user){
     const profileResponse = await fetch (`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
-    const profileData = await profileResponse.json()
 
+    const reposResponse = await fetch (`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profileData = await profileResponse.json();
+    const reposData = await reposResponse.json();
     return {
-      profile: profileData
+      profile: profileData,
+      repos: reposData
     }
   }
 }
--- ui.js
+++ ui.js
@@ -4,16 +4,16 @@ class UI {
   }
 
   showProfile(user) {
-    console.log(user)
     this.profile.innerHTML = `
     <div class="card card-body mb-3">
       <div class="row">
         <div class="col-md-3">
           <img class="img-fluid mb-2" src="${user.avatar_url}">
-          <a class="btn btn-info btn-block" href="${user.html_url}">View Profile</a>
+          <a class="btn btn-info btn-block" href="${user.html_url}" target="_blank">View Profile</a>
         </div>
         <div class="col-md-9">
-          <h3 class="text-white">${user.name}</h3>
+          <h3 class="text-white">${user.login}</h3>
+          <h5>${user.name}</h4>
           <span class="badge badge-primary">Public Repositories: ${user.public_repos}</span>
           <span class="badge badge-light">Public Gists: ${user.public_gists}</span>
           <span class="badge badge-success">Followers: ${user.followers}</span>
@@ -57,5 +57,28 @@ class UI {
     if (currentAllert) {
       currentAllert.remove()
     }
+  }
+
+  showRepose(repos) {
+    let output ='';
+    console.log(repos)
+    repos.forEach(function(repo){
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" class="list-group-item text-white" target=_blank> ${repo.name} </a>
+            </div>
+            <div class="col-md-6">
+            <span class="badge badge-warning">Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-success">Watchers: ${repo.watchers_count}</span>
+            <span class="badge badge-primary">Forks: ${repo.forks_count}</span>
+            <span class="badge badge-info"> ${repo.language}</span>
+            </div>
+          </div>
+        </div>
+      `
+    });
+    document.getElementById('repos').innerHTML = output
   }
 }
